5e960bdbdef544a5386f0c21e7cedd586a42a059
wesvr <44432187+wesvr@users.noreply.github.com>
2019-05-06 14:03:22 +0200

    Update Alabdulwahhab_VanPuyenbroeck_Vrancken_BMP_Color_Invert.c
    -Library pragma pack (1) toegevoegd.
    -Struct geplaatst.
    -54 delig in array geplaatst.
    -Reactie: hoogte en breedte is waar te nemen in pixels.

--- Alabdulwahhab_VanPuyenbroeck_Vrancken_BMP_Color_Invert.c
+++ Alabdulwahhab_VanPuyenbroeck_Vrancken_BMP_Color_Invert.c
@@ -13,15 +13,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define LENGTE  270054 
+#pragma pack (1)
+typedef struct {    		          // Total: 54 bytes
+  unsigned short  	type;             // Magic identifier: 0x4d42
+  unsigned int  	size;             // File size in bytes
+  unsigned short  	reserved1;        // Not used
+  unsigned short  	reserved2;        // Not used
+  unsigned int  	offset;           // Offset to image data in bytes from beginning of file (54 bytes)
+  unsigned int  	dib_header_size;  // DIB Header size in bytes (40 bytes)
+  int 				width_px;         // Width of the image
+  int   			height_px;        // Height of image
+  unsigned short  	num_planes;       // Number of color planes
+  unsigned short  	bits_per_pixel;   // Bits per pixel
+  unsigned int  	compression;      // Compression type
+  unsigned int  	image_size_bytes; // Image size in bytes
+  int   			x_resolution_ppm; // Pixels per meter
+  int   			y_resolution_ppm; // Pixels per meter
+  unsigned int  	num_colors;       // Number of colors  
+  unsigned int  	important_colors; // Important colors 
+} BMPHeader;
 
 
 int main(void)
-{
+{	int teller=0;
+	int array [54];
 	FILE *pointer = fopen("bmpwatamote.bmp","rb");
 	FILE *invers = fopen("invers.bmp","wb");
 
-	char a = 0;
-	
+	int a = 0;
+	printf("%d\n",sizeof(BMPHeader));
 	 if(pointer == NULL)
     {
         
@@ -36,12 +56,14 @@ int main(void)
 
     for (int i = 0; i < 54; i++)
 	{
+		teller++; 
        
         a = fgetc(pointer); // leest de karakters stukje per stukje
-
+		array[i] = a ;
         fputc(a,invers); // zet de gelezen karakters in de nieuwe file
-		
-    } 
+		printf("teller: %d\narray: %d\n",teller,array[i]);
+    } 	
+	printf("breedte: %d\nhoogte: %d\n");
 	
 	 for (int i = 54; i < LENGTE; i++)
 	{
